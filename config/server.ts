7fd90c0f718800cd67c8c1e825112b9c1f9960d5
Muratcan İlhan <99173192+muratcanilhann@users.noreply.github.com>
2024-12-09 21:08:20 +0300

    strapi added

--- config/server.ts
+++ config/server.ts
@@ -1,31 +1,7 @@
-export default () => ({
-  host: '0.0.0.0', 
-  port: 1337, 
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: [
-      'hg6oI/XBm86SzyP4cE8O3g==',
-      'll1OxUQTwx5jA4SsCbXtkg==',
-      'mmX6cHf/9bBumHgCT86C7w==',
-      'Y/vL7hohOlWCgB1Zh2nGiw==',
-    ],
-  },
-  admin: {
-    auth: {
-      secret: 'nEsChj5GTkDrFbw0eQrcvQ==', 
-    },
-  },
-  database: {
-    client: 'sqlite', 
-    connection: {
-      filename: '.tmp/data.db',
-    },
-    useNullAsDefault: true, 
-  },
-  security: {
-    apiTokenSalt: 'cgHQGFOaTqanLUo6JxJ9Xw==', 
-    transferTokenSalt: 'mHoKBtQxXSNIfffgnDFoSQ==',
-  },
-  jwt: {
-    secret: 'HULH9+DsOm+EROA+JXmmow==', 
+    keys: env.array('APP_KEYS'),
   },
 });
